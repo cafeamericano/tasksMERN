d5b1d4e115264867d24dea644f3a5e514e1bd8d1
Matthew <mfarmer5102@gmail.com>
2019-09-10 00:56:00 -0400

    AddTaskModal can now be opened and closed via the UI

--- src/AddTaskModal.js
+++ src/AddTaskModal.js
@@ -4,8 +4,15 @@ class AddTaskModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      visible: false
+      visible: this.props.visibility
     };
+  }
+
+  componentDidUpdate() {
+    let x = this.props.visibility;
+    if (x !== this.state.visible) {
+      this.setState({ visible: x });
+    }
   }
 
   render() {
@@ -13,7 +20,47 @@ class AddTaskModal extends Component {
     if (this.state.visible) {
       return (
         <div className="modal-background-react">
-          <div className="modal-foreground-react">banana</div>
+          <div className="modal-foreground-react">
+            {/* Begin Modal Content */}
+            <div class="modal-header">
+              <h5 class="modal-title">Add a Task</h5>
+            </div>
+            <div class="modal-body">
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">
+                  Email
+                </label>
+                <div class="col-sm-10">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Task name"
+                  />
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-2 col-form-label">
+                  List Name
+                </label>
+                <div class="col-sm-10">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="List"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" onClick={this.props.toggleAddTaskModal}>
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+            {/* End Modal Content */}
+          </div>
         </div>
       );
     } else {
--- src/Main.js
+++ src/Main.js
@@ -7,23 +7,42 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeList: "All Tasks"
+      activeList: "All Tasks",
+      visible_AddTaskModal: false
     };
     this.setActiveList = this.setActiveList.bind(this);
+    this.toggleAddTaskModal = this.toggleAddTaskModal.bind(this);
   }
 
   setActiveList(listName) {
     this.setState({ activeList: listName });
   }
 
+  toggleAddTaskModal() {
+    if (this.state.visible_AddTaskModal) {
+      this.setState({ visible_AddTaskModal: false });
+    } else {
+      this.setState({ visible_AddTaskModal: true });
+    }
+  }
   render() {
     return (
       <main className="row full-height">
-        <AddTaskModal/>
+        <AddTaskModal
+          visibility={this.state.visible_AddTaskModal}
+          toggleAddTaskModal={this.toggleAddTaskModal}
+        />
         <div className="col-3 p-3 bg-primary text-light">
           <h4>Tasks MERN</h4>
           <hr></hr>
-          <Sidepanel makeActiveList={this.setActiveList} activeList={this.state.activeList}/>
+          <div className="text-center" onClick={this.toggleAddTaskModal}>
+            <i class="fas fa-plus-circle fa-3x"></i>
+          </div>
+          <hr></hr>
+          <Sidepanel
+            makeActiveList={this.setActiveList}
+            activeList={this.state.activeList}
+          />
         </div>
         <div className="col-9 p-5 bg-light">
           <Taskbox activeList={this.state.activeList} />
