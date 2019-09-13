60441f6534838d8b004b176460316420214d42a3
Matthew <mfarmer5102@gmail.com>
2019-09-13 17:37:38 -0400

    Adding path as a requirement in server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
 const cors = require("cors");
+var path = require("path");
 var app = express();
 
 var PORT = process.env.PORT || 7000;
